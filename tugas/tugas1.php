494273d1e5f3214b9654d24b2fbffc6422b3a070
hiruma48 <49358000+hiruma48@users.noreply.github.com>
2023-09-14 20:48:58 +0700

    tugas 1 php
    kumpul tugas

---
+++ tugas/tugas1.php
@@ -0,0 +1,16 @@
+<?php
+echo"<h1>Daftar Menu</h1>";
+$makanan="
+    <li>ayam goreng = Rp. 13000</li>
+    <li>ayam bakar   = Rp. 15000</li>
+    <li>ayam sayur   = Rp. 13000</li>
+    <li>tempe goreng = Rp. 1000</li>
+    <li>tahu goreng  = Rp. 1000</li>
+    <li>soto         = Rp. 3000</li>
+    <li>nasi putih   = Rp. 5000</li>
+    <li>es teh       = Rp. 4000</li>
+    <li>es tebu      = Rp. 5000</li>
+
+";
+echo"$makanan"
+?>
---
+++ tugas/tugas1_2.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html>  
+    <style>
+        table,th,td{
+            border: 1px solid black;
+        }
+    </style>
+<body>
+    <?php
+    echo"<h1>DAFTAR MENU</h1>";
+   $row = '<table style="width:20%">
+    <tr>
+        <th>no</th>
+        <th>Nama Menu</th>
+        <th>harga</th>
+    </tr>
+
+   <tr>
+        <td>1</td>
+        <td>cireng</td>
+        <td>Rp1000</td>
+    </tr>
+   <tr>
+        <td>2</td>
+        <td>es duren</td>
+        <td>Rp 15000</td>
+        </tr>
+   <tr>
+        <td>3</td>
+        <td>Es Milo</td>
+        <td>Rp 20000</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>es teh</td>
+        <td>Rp 5000</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>piscok</td>
+        <td>Rp 1000</td>
+    </tr>
+    </table>
+
+   ' ;
+    echo"$row"
+
+    
+    ?>
+
+
+</body>
+</html>
